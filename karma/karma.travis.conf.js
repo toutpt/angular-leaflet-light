cb04d75cf0af4e5d929eb75821b91e3534e1732b
JeanMichel FRANCOIS <toutpt@gmail.com>
2016-05-27 16:17:27 +0200

    chore(test): fix coverage

--- karma/karma.travis.conf.js
+++ karma/karma.travis.conf.js
@@ -3,7 +3,7 @@ var conf = require('./conf');
 module.exports = function(config){
     conf.browsers = ['PhantomJS'];
     conf.reporters.push('coverage');
-    conf.preprocessors['../dist/angular-leaflet.js'] = ['coverage'];
+    conf.preprocessors['../src/*.js'].push('coverage');
     config.singleRun = true;
     conf.coverageReporter.type = 'lcov';
     config.set(conf);
